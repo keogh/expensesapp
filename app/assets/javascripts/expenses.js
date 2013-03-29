860ead1a56e1f1096e8e4ba320b95f501b06e113
Isaac Zepeda <keogh24@gmail.com>
2013-03-29 14:20:35 -0700

    Initial commit

---
+++ app/assets/javascripts/expenses.js
@@ -0,0 +1,5 @@
+app.controller('ExpensesController', ['$scope', function($scope) {
+
+	
+
+}]);
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expensesapp::Application.config.secret_token = '39a8d134836a7fe724e5d20e84fb6b24c7e5d08f4f9b3d9a593acb20945b2d1970f5aabb26f3be5b82d6075aae5c4b642a4327d38dce97a3c613354fc4756b9f'
