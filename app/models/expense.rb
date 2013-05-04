cdc2b50d4ffc0ac78ebd906c2f5ac0ea1be41125
Isaac Zepeda <keogh24@gmail.com>
2013-05-04 12:08:24 -0600

    Model Expense associations to User model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ActiveRecord::Base
   attr_accessible :date, :name, :owner_id, :creator_id, :price
+
+  belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
 end
